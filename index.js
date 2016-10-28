1de9b712059275a96219cd194418ef1dfcd934ce
Benji Kay <okaybenji@gmail.com>
2016-10-28 16:39:59 -0500

    allow moving player with arrow keys

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let player = createEntity({
 
 player.move = (dir) => {
   const movePlayer = ({x = 0, y = 0}) => {
-    return moveEntityBy({entity: player, x, y})
+    return moveEntityBy({entity: player, x, y});
   };
   const vel = 1;
   const directions = {
@@ -43,12 +43,67 @@ player.move = (dir) => {
   return directions[dir]();
 };
 
+let input = {
+  n: false,
+  s: false,
+  e: false,
+  w: false,
+  atk: false
+};
+
+function updateInput(keyCode, val) {
+  const newInput = function(keyCode) {
+    switch (keyCode) {
+      // left
+      case 65:
+      case 37:
+        return {w: val};
+
+      // up
+      case 87:
+      case 38:
+        return {n: val};
+
+      // right
+      case 68:
+      case 39:
+        return {e: val};
+
+      // down
+      case 83:
+      case 40:
+        return {s: val};
+
+      // space bar;
+      case 32:
+        return {atk: val};
+    }
+  };
+
+  return Object.assign({}, input, newInput(keyCode));
+}
+
+document.addEventListener('keydown', (e) => {
+  input = updateInput(e.keyCode, true);
+});
+
+document.addEventListener('keyup', (e) => {
+  input = updateInput(e.keyCode, false);
+});
+
 const loop = () => {
   // clear canvas
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
 
   // draw bg
   ctx.drawImage(arena, -80, -156);
+
+  // update player
+  for (const dir in input) {
+    if (input[dir]) {
+      player = player.move(dir);
+    }
+  }
 
   // draw player
   ctx.drawImage(player.img, player.x, player.y);
